06f3314d9939777ff966bdebfb3fc2018d8bd9d3
githubsivaragu <sivaragubeece@gmail.com>
2022-08-02 21:45:35 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,96 @@
+import logo from './icons/logo.jpg';
+//import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+import Home  from './components/Home.js'
+import Contact  from './components/Contact.js'
+import Orders  from './components/Orders.js'
+import './App.css';
+import { Component } from 'react';
+
+class App extends Component{
+
+
+   constructor(prop)
+   {
+    super()
+
+    this.state={
+      Home:'Home',
+      Contact:'Contact',
+      Orders:'orders'
+    }
+    this.state.pagename="Home";
+    this.Routerlinkpage = this.Routerlinkpage.bind(this);
+
+
+
+   }
+   Routerlinkpage(props)
+   {
+    this.setState({pagename:props.target.title})
+    }
+    renderElement(){
+      if(this.state.pagename === 'Contact')
+         return (<Contact/>);
+         if(this.state.pagename === 'orders')
+         return (<Orders/>);
+      return  (<Home/>);
+    }
+    
+   
+  render(){
+const marginheader={
+  height: '58px',
+  border: 'solid aliceblue',
+  boxShadow: "3px 3px 3px 3px cadetblue",
+  position: "fixed",
+  left: 0,
+  top: 0,
+  width: "100%",
+ backgroundColor: "white",
+ // color: "white"
+}
+
+const  headerpadding={
+  
+    paddingRight:'33px',
+    cursor: "pointer"
+}
+
+const footer= {
+  position: "fixed",
+  left: 0,
+  bottom: 0,
+  width: "100%",
+  backgroundColor: "black",
+  color: "white",
+  textAlign: "center"
+}
+  return (
+    <div>
+          <header style={marginheader}>
+            <h2  className="split left">
+              <img src={logo} style={{height:"40px"}}></img>
+            <span >AF Menu</span>            
+            </h2>
+            <h3 className="split right">           
+            <div  style={{ margin: '10px',textDecoration: "none",textAlign: "right" }}>
+                        <span style={ headerpadding} title={this.state.Home} onClick={this.Routerlinkpage }>Home</span>
+                        <span style={ headerpadding} title={this.state.Contact} onClick={this.Routerlinkpage }>Contact's</span>  
+                        <span style={ headerpadding} title={this.state.Orders} onClick={this.Routerlinkpage }>Orders</span>                    
+            </div>
+           </h3>
+        </header>
+           <div  id="Home" style={{ paddingTop: '70px'}}>
+              {this.renderElement()}
+           </div>
+            
+      <div style={footer}>
+     <h5>© Copyright 2022 American Fresh Menu Services All Rights Reserved</h5>
+      </div>
+    
+    </div>
+  );
+}
+}
+
+export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,212 @@
+import React , { Component } from 'react';
+//import log   from "../images/4.jpg";
+import Popup from 'reactjs-popup'
+class Home extends Component
+{
+ constructor()
+ {
+super();
+this.state={Itemid:"0"};
+this.CallingItems=this.CallingItems.bind(this)
+this.jsondata=[{
+  Id:1,
+  Item:"Juice",
+  Image:require("../images/1.jpg")
+  },
+  {
+    Id:2,
+    Item:"Veg",
+    Image:require("../images/2.jpg")
+    },
+    {
+  
+      Id:3,
+      Item:"Non-Veg",
+      Image:require("../images/3.jpg")
+    },
+    {
+  
+        Id:4,
+        Item:"Milk Items",
+        Image:require("../images/4.jpg")
+    }
+  ];
+
+  this.JuiceItems=[
+    {
+      Id:1,
+      Item:"Apple Fresh",
+      Image:require("../images/6.jpg"),
+      Price:5,
+      Currency:"$",
+      Require:{
+        Item1:"Mix With cherry",
+        Item2:"More Suger"
+      },
+      NotRequire:{
+        Item1:"Ice",
+        Item2:"More Suger"
+      }
+      },
+      {
+        Id:1,
+        Item:"Avocado  Fresh",
+        Image:require("../images/6.jpg"),
+        Price:5,
+        Currency:"$",
+        Require:{
+          Item1:"Mix With cherry",
+          Item2:"More Suger"
+        },
+        NotRequire:{
+          Item1:"Ice",
+          Item2:"More Suger"
+        }
+        }
+    ];
+
+
+
+
+ }
+
+
+
+elements(){
+  const marginheader={
+    height: '400px',
+    border: 'solid aliceblue',
+    boxShadow: "3px 3px 3px 3px cadetblue",
+    width: "50%",
+    position: "fixed",
+   backgroundColor: "white",
+   // color: "white"
+  }
+  const center= {
+    margin: "auto",
+    width: "60%",
+    padding: "10px",
+  }
+  const detailimg={
+    display: "flex",
+    justifyContent: "end",
+    float:"left",width:"36%", padding:"1%",position:"right center"
+  }
+  const w3Quarter1={
+  
+    float:"left",width:"100%",padding:"1%",position:"right center" ,boxShadow: "3px 3px 3px 3px cadetblue",visibility:"show"
+    ,height: "175px"
+ 
+  }
+  const fontWeight={
+  
+    float:"left",width:"100%",padding:"2%",position:"right center" 
+  }
+return <>
+
+{this.JuiceItems.map((Ite)=>{
+  return(
+    <>
+<div style={center}>
+   <div style={w3Quarter1} >
+     <div  style={detailimg}>
+     
+   <img src={Ite.Image} alt="Sandwich" style={{width:"100%" ,height:"150px"   }}/> 
+   </div>
+      
+      <h3 style={{textAlign:"center",margin:"2px"}} >{Ite.Item}</h3>
+     <h5 style={{margin:"1px"}}>Required</h5>
+     {this.ItemrequireornotRequire(Ite.Require)}
+      <div>
+     <h5 style={{margin:"1px"}}>Not Required</h5>
+       {this.ItemrequireornotRequire(Ite.NotRequire)}
+     
+       <span  style={{paddingLeft:"200px", fontWeight: "bold",fontSize: "larger",  color: "green"}} >Price:- {Ite.Price}$ </span>  
+       <input type="button" value="ADD+" /> 
+      </div>
+     </div>
+    </div>
+   
+</>
+  )})}
+   <div style={fontWeight}>
+     
+  </div>
+  </>
+}
+
+ItemrequireornotRequire(Ite)
+{
+
+  const lablelAlign={
+    fontWeight: "bold",
+fontSize: "larger",
+padding: "5px"
+  }
+debugger;
+     return(
+       <>
+       <label  style={lablelAlign}>{Ite.Item1}</label>
+       <input type="checkBox"/>
+       <label  style={lablelAlign}>{Ite.Item2}</label>
+       <input type="checkBox"/>
+       </>       
+ )
+}
+
+CallingItems(e)
+{
+debugger;
+this.setState({Itemid:e.target.id});
+this.RenderElement();
+e.preventDefault();
+}
+
+
+  RenderElement()
+ {
+
+ 
+  const w3Quarter={
+    float:"left",width:"25%", boxShadow:"2px 3px 5px brown",padding:"1%",position:"right center "
+  }
+
+  return < >
+  {this.jsondata.map((e)=>{ 
+  return   ( <div style={w3Quarter} key={e.Id}>
+<img src={e.Image} alt="Sandwich" style={{width:"100%"    }} id={e.Id} onClick={this.CallingItems} />   
+   <h3 style={{textAlign:"center"}} >{e.Item}</h3>
+  <p>Just some random text, lorem ipsum text praesent tincidunt ipsum lipsum.</p>
+</div>
+  ) }) }
+    
+     {this.state.Itemid==="0" || this.state.Itemid===undefined?<span className="sr-only">Loading... Registered Devices</span>: this.elements()}
+    
+  </>
+ };
+
+
+   render()
+   {
+
+const Style={
+  margin:"60px"
+}
+
+    return (
+      <>
+       <div style={Style}>
+       {this.RenderElement()}
+       </div>
+        
+       </>
+  )
+   }
+  }
+
+ 
+  export default Home;
+
+
+
+  
